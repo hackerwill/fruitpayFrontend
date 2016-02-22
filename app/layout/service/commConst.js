65f1246b0e401646b1540e7eb3ce4339e10745ab
Will <u9734017@gmail.com>
2016-02-22 12:24:10 +0800

    change router setting full url

--- app/layout/service/commConst.js
+++ app/layout/service/commConst.js
@@ -9,6 +9,7 @@ function commConst(){
 	 */
 	service.SERVER_DOMAIN = "${GULP_SERVER_DOMAIN}";
 	service.CLINET_DOMAIN = "${GULP_CLIENT_DOMAIN}";
+	service.CLINET_FULL_DOMAIN = "${GULP_CLIENT_DOMAIN}" + "${GULP_BASE_HREF}".substring(1);
 	
 	service.DAY_OF_WEEK = {
 		1 : '一',
@@ -30,42 +31,42 @@ function commConst(){
 	service.urlState = {
 		INDEX : {
 			stateName : 'app',
-			fullUrl: "${GULP_BASE_HREF}" + 'app',
+			fullUrl: service.CLINET_FULL_DOMAIN + 'app',
             url: "/app",
             templateUrl: 'layout/shell.html',
 			controller:'shellController'
         },
 		CHECKOUT : {
 			stateName : 'app.checkout',
-			fullUrl: "${GULP_BASE_HREF}" + "app/checkout",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/checkout",
             url: "/checkout",
             templateUrl: 'checkout/checkout.html',
             controller:'checkoutController'
         },
 		LOGIN : {
 			stateName : 'app.login',
-			fullUrl: "${GULP_BASE_HREF}" + "app/login",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/login",
             url: "/login",
             templateUrl: 'login/login.html',
             controller:'loginController'
         },
 		LOGOUT : {
 			stateName : 'app.logout',
-			fullUrl: "${GULP_BASE_HREF}" + "app/logout",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/logout",
             url: "/logout",
             templateUrl: 'login/logout.html',
             controller:'logoutController'
         },
 		FORGET_PASSWORD : {
 			stateName : 'app.forgetPassword',
-			fullUrl: "${GULP_BASE_HREF}" + "app/forgetPassword",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/forgetPassword",
             url: "/forgetPassword",
             templateUrl: 'login/forgetPassword.html',
 			controller:'forgetPasswordController',
         },
 		USER : {
 			stateName : 'app.user',
-			fullUrl: "${GULP_BASE_HREF}" + "app/user",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/user",
             url: "/user",
             templateUrl: 'user/user.html',
             controller:'userController',
@@ -73,7 +74,7 @@ function commConst(){
         },
 		ORDERS : {
 			stateName : 'app.user.orders',
-			fullUrl: "${GULP_BASE_HREF}" + "app/user/orders",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/user/orders",
             url: "/orders",
             templateUrl: 'user/order.html',
             controller:'orderController',
@@ -81,7 +82,7 @@ function commConst(){
         },
 		INFO : {
 			stateName : 'app.user.info',
-			fullUrl: "${GULP_BASE_HREF}" + "app/user/info",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/user/info",
 			url: "/info",
             templateUrl: 'user/info.html',
             controller:'infoController',
@@ -89,7 +90,7 @@ function commConst(){
         },
 		CHECKOUT_CREDIT_CARD_SUCCESS : {
 			stateName : 'app.checkoutCreditCardSuccess',
-			fullUrl: "${GULP_BASE_HREF}" + "app/checkoutCreditCardSuccess",
+			fullUrl: service.CLINET_FULL_DOMAIN + "app/checkoutCreditCardSuccess",
 			url: "/checkoutCreditCardSuccess",
             templateUrl: 'checkout/checkoutCreditCardSuccess.html'
 		}
