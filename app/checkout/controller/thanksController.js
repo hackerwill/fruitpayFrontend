8bcb0065ac515d95570274802f1d3438cabf6711
Will <u9734017@gmail.com>
2016-02-29 18:36:38 +0800

    test ecommerce setting

--- app/checkout/controller/thanksController.js
+++ app/checkout/controller/thanksController.js
@@ -4,7 +4,10 @@ angular.module('checkout')
 			["$scope", "orderService", "spinService", 
 		function(
 				$scope, orderService, spinService){
-					
+		
+		if(!getURLParameter('id'))
+			return;
+		
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -13,10 +16,7 @@ angular.module('checkout')
 		ga('create', 'UA-54050037-3', 'auto');
 		ga('send', 'pageview');
 		
-		ga('require', 'ecommerce');
-		
-		if(!getURLParameter('id'))
-			return;
+		ga('require', 'ecommerce', 'ecommerce.js');
 		
 		$scope.finished = false;
 		spinService.startSpin("系統處理中，請稍後");
@@ -27,7 +27,7 @@ angular.module('checkout')
 				if(result){
 					$scope.finished = true;
 					ga('ecommerce:addTransaction', { 
-						'id': result.orderId, // Transaction ID. Required. 
+						'id': '"' + result.orderId + '"', // Transaction ID. Required. 
 						'affiliation': 'Fruitpay', // Affiliation or store name. 
 						'revenue': result.totalPrice, // Grand Total. 
 						'shipping': result.shippingCost, // Shipping. 
@@ -36,9 +36,9 @@ angular.module('checkout')
 					}); 
 
 					ga('ecommerce:addItem', { 
-						'id': result.orderId, // Transaction ID. Required. 
+						'id': '"' + result.orderId + '"', // Transaction ID. Required. 
 						'name': result.orderProgram.programName, // Product name. Required. 
-						'sku': result.orderProgram.programId, // SKU/code. 
+						'sku': '"' + result.orderProgram.programId + '"', // SKU/code. 
 						'category': 'fruitBox', // Category or variation. 
 						'price': result.orderProgram.price, // Unit price. 
 						'quantity': result.programNum // Quantity. 
